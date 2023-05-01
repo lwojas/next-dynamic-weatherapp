366e28a1ba91bc4137a9defb8cbfae1d08b8965f
lwojas <lechwojas@gmail.com>
2023-05-01 14:34:09 +0200

    Initial commit - cloned weather app

---
+++ src/libs/changeBackground.ts
@@ -0,0 +1,16 @@
+export const changeBackground = (iconKey: number, temp: number) => {
+  let cssClass = "";
+  // TODO: Put these class strings in an array with indexes matching the icon numbers
+  if (iconKey < 4) {
+    if (temp > 27) {
+      cssClass = "bg-sunny";
+    } else {
+      cssClass = "bg-fair";
+    }
+  } else if (iconKey < 7) {
+    cssClass = "bg-mild";
+  } else if (iconKey < 14) {
+    cssClass = "bg-cloudy";
+  }
+  return cssClass;
+};
