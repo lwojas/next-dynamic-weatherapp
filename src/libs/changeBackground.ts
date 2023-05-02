cc15de1f905edda14c190d171e9955a7c0d862ec
lwojas <lechwojas@gmail.com>
2023-05-02 12:57:57 +0200

    fix: Simplified logic for background changes according to icon numbers, uses css classes with numeric names that match the icon number eg. iconNumber = 2; css class = ._2 - Lech

--- src/libs/changeBackground.ts
+++ src/libs/changeBackground.ts
@@ -1,18 +1,8 @@
 export const changeBackground = (iconKey: number, temp: number) => {
-  let cssClass = "";
-  // TODO: Put these class strings in an array with indexes matching the icon numbers
-  if (iconKey < 4) {
-    if (temp > 27) {
-      cssClass = "bg-sunny";
-    } else {
-      cssClass = "bg-fair";
-    }
-  } else if (iconKey < 7) {
-    cssClass = "bg-mild";
-  } else if (iconKey < 14) {
-    cssClass = "bg-cloudy";
-  } else if (iconKey > 31) {
-    cssClass = "bg-night";
+  let cssClass = "_" + iconKey;
+  if (iconKey < 4 && temp > 27) {
+    // Special class for very hot sunny days
+    cssClass = "bg-sunny";
   }
   return cssClass;
 };
